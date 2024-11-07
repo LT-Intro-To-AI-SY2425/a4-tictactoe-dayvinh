69c479f7bb49d3f4211b5c3584dc70cdbdf91288
Davin Nguyen <davin.nguyen2017@gmail.com>
2024-11-07 10:10:03 -0600

    Finished!

--- notes.py
+++ notes.py
@@ -2,3 +2,19 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    def __init__(self, n = "no name", a = 0):
+        self.name = n
+        self.age = a
+        
+    def __str__(self):
+        s = f"{self.name} is {self.age} years old"
+        return s
+
+logan = Dog("logan", 8)
+becker = Dog("becker", 4)
+kepa = Dog("kepa", 4)
+
+print(logan)
+print(becker)
+print(kepa)
